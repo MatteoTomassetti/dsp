1e19508c0235f68fa049f532ece436715be4bb3f
Matteo Tomassetti <m.tomassetti@gmail.com>
2015-12-15 16:51:55 +0200

    coded the solution

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,7 +15,7 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    return len(filter(lambda x:x[0]==x[-1] if len(x)>=2 else False,words))
 
 
 def front_x(words):
@@ -32,7 +32,10 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+    start_with_x = [item for item in words if item[0]=="x"]
+    no_start_with_x = [item for item in words if item[0]!="x"]
+    
+    return sorted(start_with_x) + sorted(no_start_with_x)
 
 
 def sort_last(tuples):
@@ -49,7 +52,7 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    return sorted(tuples,key=lambda elements:elements[-1])
 
 
 def remove_adjacent(nums):
@@ -68,7 +71,11 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+    for i in range(len(nums)-1,0,-1):
+        if(nums[i] == nums[i-1]):
+            del nums[i]
+    
+    return nums
 
 
 def linear_merge(list1, list2):
@@ -85,4 +92,4 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    return sorted(list1+list2)
