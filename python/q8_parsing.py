924271c8979619dc4e2cac8740c0e86d804ff756
Matteo Tomassetti <m.tomassetti@gmail.com>
2015-12-15 18:24:48 +0200

    solved the challenge

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -9,11 +9,31 @@
 
 import csv
 
-  def read_data(data):
-   # COMPLETE THIS FUNCTION
+class Football:
+  
+  def __init__(self,data):
+    reader = csv.reader(open(data, 'r'))
+    keys = reader.next()
+    self.parsed_data={key:[] for key in keys}
+    for row in reader:
+      for k in range(len(row)):
+        self.parsed_data[keys[k]].append(row[k])
+    #
+    self.parsed_data[keys[0]] = np.array(self.parsed_data[keys[0]])
+    for k in range(1,len(keys)):
+      self.parsed_data[keys[k]] = np.array(self.parsed_data[keys[k]]).astype("int")
+  
+  def get_min_score_difference(self):
+    return min(self.parsed_data["Goals"]-self.parsed_data["Goals Allowed"])
+  
+  def get_team(self):
+    mask = self.parsed_data["Goals"]-self.parsed_data["Goals Allowed"] == self.get_min_score_difference()
+    return self.parsed_data["Team"][mask][0]
 
-  def get_min_score_difference(self, parsed_data):
-    # COMPLETE THIS FUNCTION
+g=Football("football.csv")
 
-  def get_team(self, index_value, parsed_data):
-    # COMPLETE THIS FUNCTION
+print 'Team with the minimum difference between "Goals" and "Goals Allowed":', g.get_team()
+# print g.get_min_score_difference()
+  
+
+  
