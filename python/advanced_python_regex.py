3ba88676271bd16bf68242957cb98184457b6dd2
Matteo Tomassetti <m.tomassetti@gmail.com>
2015-12-20 20:46:06 +0100

    made it compatible with python 3

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -4,7 +4,7 @@ import numpy as np
 # reading the data
 datafile = "faculty.csv"
 reader = csv.reader(open(datafile, 'r'))
-keys = reader.next()
+keys = reader.__next__()
 data={key:[] for key in keys}
 for row in reader:
   for k in range(len(row)):
@@ -26,7 +26,7 @@ for dummy in data[" degree"]:
 
 from collections import Counter
 for degree, frequency in Counter(all_degrees).most_common():
-  print degree, frequency
+  print (degree, frequency)
 
 # Q2. Find how many different titles there are, and their frequencies:  Ex:  Assistant Professor, Professor
 # We want to remove the part of string after "of ..."
@@ -39,11 +39,11 @@ for title in data[" title"]:
   all_titles.append(title[:idx])
 
 for title, frequency in Counter(all_titles).most_common():
-  print title, frequency
+  print (title, frequency)
 
 # Q3. Search for email addresses and put them in a list.  Print the list of email addresses.
 all_emails = data[" email"]
-print all_emails.tolist()
+print (all_emails.tolist())
 
 # Q4. Find how many different email domains there are (Ex:  mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.).  Print the list of unique email domains.
 
@@ -52,6 +52,6 @@ for email in all_emails:
   idx = email.find("@") # we want to save only the domain of each email
   all_domains.append(email[idx+1:])
 
-print np.unique(np.array(all_domains))
+print (np.unique(np.array(all_domains)))
 
   
